f6e64f1999d7a9629cdb181b1b24dbb0744f3da3
Fedosov Alexander <achpile@gmail.com>
2017-07-20 22:37:46 +0300

    1001

---
+++ solutions/1001.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+
+int main() {
+	int   i;
+	double a;
+	std::vector<double> list;
+
+	while (scanf("%lf", &a) != EOF)
+		list.push_back(sqrt(a));
+
+	for (i = list.size() - 1; i >= 0; i--)
+		printf("%.4lf\n", list[i]);
+
+	return 0;
+}
