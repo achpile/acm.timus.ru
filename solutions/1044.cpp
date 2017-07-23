73d41386962e01d51e76b92ccb3c6caa6bcefc29
Fedosov Alexander <achpile@gmail.com>
2017-07-23 23:53:16 +0300

    1044

---
+++ solutions/1044.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main() {
+	int c;
+
+	scanf("%d", &c);
+
+	if (c == 2) printf("10\n");
+	if (c == 4) printf("670\n");
+	if (c == 6) printf("55252\n");
+	if (c == 8) printf("4816030\n");
+
+	return 0;
+}
